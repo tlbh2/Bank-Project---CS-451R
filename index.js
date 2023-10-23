7203cf400bb82a0cb0cb0a73f792e54ff18b1575
tlbh2 <70487263+tlbh2@users.noreply.github.com>
2023-10-22 21:25:10 -0700

    front-end update
    - website redesign to accommodate new nav bar
    - simplify footer
    - initial login and signup

--- index.js
+++ index.js
@@ -9,12 +9,25 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/index", (req, res) => {
+  res.render("index.ejs");
+});
+
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.get("/plaidChartView", (req, res) => {
+  res.render("plaidChartView.ejs");
+});
+
 app.get("/about", (req, res) => {
   res.render("about.ejs");
 });
 
-app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
+app.get("/logInSignUp", (req, res) => {
+  res.render("logInSignUp.ejs");
 });
 
 app.listen(port, () => {
