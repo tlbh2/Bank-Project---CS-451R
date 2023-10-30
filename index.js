5937f80602f7cd77a6304513db6126e9ed8bbd3b
Tin Lao <tinlaotrung@gmail.com>
2023-10-30 14:47:59 -0700

    implement Oauth
    googleId

--- index.js
+++ index.js
@@ -16,13 +16,101 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
-app.get('/', (req, res) => {
-  res.render('index.ejs')
+//initialize session
+app.use(
+  session({
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+//connect to local database by mongodb
+mongoose.connect('mongodb://127.0.0.1:27017/bankUserDB')
+//mongoose.connect("mongodb+srv://tinlao:laotrungtin@cluster0.oxpatx9.mongodb.net/todolistDB");
+
+//an object that created from Schema class of mongodb
+const userSchema = new mongoose.Schema({
+  name: String,
+  userName: String,
+  email: String,
+  password: String,
+  facebookId: String,
+  googleId: String,
+  twitterId: String,
+  github: String,
 })
 
+userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
+
+const User = new mongoose.model('User', userSchema)
+
+passport.use(User.createStrategy())
+
+//serialize and deserialize of model for passport session
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture,
+    })
+  })
+})
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user)
+  })
+})
+
+//put this before the route and after initialize session and serialize and deserialize
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/secrets', //redirect user to this URL once access is granted (or denied)
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile)
+
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user)
+      })
+    }
+  )
+)
+
+app.get('/', (req, res) => {
+  res.render('home.ejs')
+})
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }))
+
+//make sure the link match Authorized redicet URLs on google APIs account
+app.get(
+  '/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect('/secrets')
+  }
+)
+
 app.get('/index', (req, res) => {
-  res.render('index.ejs')
+  res.render('home.ejs')
 })
 
 app.get('/contact', (req, res) => {
