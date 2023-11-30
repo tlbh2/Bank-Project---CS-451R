889d5377c7a0bd76364ce6382e96e12b6a35a764
Tin Lao <70487263+tlbh2@users.noreply.github.com>
2023-11-29 18:25:31 -0600

    fixing profilePic

--- index.js
+++ index.js
@@ -15,6 +15,295 @@ const findOrCreate = require('mongoose-findorcreate')
 const app = express()
 const port = 3000
 
+//-----Plaid-start------------------------------------------//
+const plaid = require('plaid');
+const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
+const util = require('util');
+const { v4: uuidv4 } = require('uuid');
+const moment = require('moment');
+const cors = require('cors');
+
+const APP_PORT = port;
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
+const PLAID_SECRET = process.env.PLAID_SECRET;
+const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
+
+const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || Products.Transactions).split(
+  ',',
+);
+
+const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(
+  ',',
+);
+
+let ACCESS_TOKEN = null;
+let PUBLIC_TOKEN = null;
+let ITEM_ID = null;
+let ACCOUNT_ID = null;
+let PAYMENT_ID = null;
+let AUTHORIZATION_ID = null;
+let TRANSFER_ID = null;
+
+// Initialize the Plaid client
+// Find the API keys in the Dashboard (https://dashboard.plaid.com/account/keys)
+const configuration = new Configuration({
+  basePath: PlaidEnvironments[PLAID_ENV],
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': PLAID_CLIENT_ID,
+      'PLAID-SECRET': PLAID_SECRET,
+      'Plaid-Version': '2020-09-14',
+    },
+  },
+});
+
+const client = new PlaidApi(configuration);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/api/info', function (request, response, next) {
+  response.json({
+    item_id: ITEM_ID,
+    access_token: ACCESS_TOKEN,
+    products: PLAID_PRODUCTS,
+  });
+});
+
+// Create a link token with configs which we can then use to initialize Plaid Link client-side.
+app.post('/api/create_link_token', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+      const configs = {
+        user: {
+          // This should correspond to a unique id for the current user.
+          client_user_id: 'user-id',
+        },
+        client_name: 'Plaid Quickstart',
+        products: PLAID_PRODUCTS,
+        country_codes: PLAID_COUNTRY_CODES,
+        language: 'en',
+      };
+
+      if (PLAID_REDIRECT_URI !== '') {
+        configs.redirect_uri = PLAID_REDIRECT_URI;
+      }
+
+      if (PLAID_ANDROID_PACKAGE_NAME !== '') {
+        configs.android_package_name = PLAID_ANDROID_PACKAGE_NAME;
+      }
+      const createTokenResponse = await client.linkTokenCreate(configs);
+      prettyPrintResponse(createTokenResponse);
+      response.json(createTokenResponse.data);
+    })
+    .catch(next);
+});
+
+// Create a link token with configs which we can then use to initialize Plaid Link client-side
+// for a 'payment-initiation' flow.
+app.post(
+  '/api/create_link_token_for_payment',
+  function (request, response, next) {
+    Promise.resolve()
+      .then(async function () {
+        const createRecipientResponse =
+          await client.paymentInitiationRecipientCreate({
+            name: 'Harry Potter',
+            iban: 'GB33BUKB20201555555555',
+            address: {
+              street: ['4 Privet Drive'],
+              city: 'Little Whinging',
+              postal_code: '11111',
+              country: 'GB',
+            },
+          });
+        const recipientId = createRecipientResponse.data.recipient_id;
+        prettyPrintResponse(createRecipientResponse);
+
+        const createPaymentResponse =
+          await client.paymentInitiationPaymentCreate({
+            recipient_id: recipientId,
+            reference: 'paymentRef',
+            amount: {
+              value: 1.23,
+              currency: 'GBP',
+            },
+          });
+        prettyPrintResponse(createPaymentResponse);
+        const paymentId = createPaymentResponse.data.payment_id;
+
+        // We store the payment_id in memory for demo purposes - in production, store it in a secure
+        // persistent data store along with the Payment metadata, such as userId.
+        PAYMENT_ID = paymentId;
+
+        const configs = {
+          client_name: 'Plaid Quickstart',
+          user: {
+            // This should correspond to a unique id for the current user.
+            // Typically, this will be a user ID number from your application.
+            // Personally identifiable information, such as an email address or phone number, should not be used here.
+            client_user_id: uuidv4(),
+          },
+          // Institutions from all listed countries will be shown.
+          country_codes: PLAID_COUNTRY_CODES,
+          language: 'en',
+          // The 'payment_initiation' product has to be the only element in the 'products' list.
+          products: [Products.PaymentInitiation],
+          payment_initiation: {
+            payment_id: paymentId,
+          },
+        };
+        if (PLAID_REDIRECT_URI !== '') {
+          configs.redirect_uri = PLAID_REDIRECT_URI;
+        }
+        const createTokenResponse = await client.linkTokenCreate(configs);
+        prettyPrintResponse(createTokenResponse);
+        response.json(createTokenResponse.data);
+      })
+      .catch(next);
+  },
+);
+
+// Exchange token flow - exchange a Link public_token for an API access_token
+app.post('/api/set_access_token', function (request, response, next) {
+  PUBLIC_TOKEN = request.body.public_token;
+  Promise.resolve()
+    .then(async function () {
+      const tokenResponse = await client.itemPublicTokenExchange({
+        public_token: PUBLIC_TOKEN,
+      });
+      prettyPrintResponse(tokenResponse);
+      ACCESS_TOKEN = tokenResponse.data.access_token;
+      ITEM_ID = tokenResponse.data.item_id;
+      response.json({
+        // the 'access_token' is a private token, DO NOT pass this token to the frontend in your production environment
+        access_token: ACCESS_TOKEN,
+        item_id: ITEM_ID,
+        error: null,
+      });
+    })
+    .catch(next);
+});
+// Retrieve ACH or ETF Auth data for an Item's accounts
+app.get('/api/auth', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+      const authResponse = await client.authGet({
+        access_token: ACCESS_TOKEN,
+      });
+      prettyPrintResponse(authResponse);
+      response.json(authResponse.data);
+    })
+    .catch(next);
+});
+
+// Retrieve Transactions for an Item
+app.get('/api/transactions', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+      // Set cursor to empty to receive all historical updates
+      let cursor = null;
+
+      // New transaction updates since "cursor"
+      let added = [];
+      let modified = [];
+      // Removed transaction ids
+      let removed = [];
+      let hasMore = true;
+      // Iterate through each page of new transaction updates for item
+      while (hasMore) {
+        const request = {
+          access_token: ACCESS_TOKEN,
+          cursor: cursor,
+        };
+        const response = await client.transactionsSync(request)
+        const data = response.data;
+        // Add this page of results
+        added = added.concat(data.added);
+        modified = modified.concat(data.modified);
+        removed = removed.concat(data.removed);
+        hasMore = data.has_more;
+        // Update cursor to the next cursor
+        cursor = data.next_cursor;
+        prettyPrintResponse(response);
+      }
+
+      const compareTxnsByDateAscending = (a, b) => (a.date > b.date) - (a.date < b.date);
+      // Return the 8 most recent transactions
+      const recently_added = [...added].sort(compareTxnsByDateAscending).slice(-8);
+      response.json({latest_transactions: recently_added});
+    })
+    .catch(next);
+});
+// Retrieve Identity for an Item
+app.get('/api/identity', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+      const identityResponse = await client.identityGet({
+        access_token: ACCESS_TOKEN,
+      });
+      prettyPrintResponse(identityResponse);
+      response.json({ identity: identityResponse.data.accounts });
+    })
+    .catch(next);
+});
+
+// Retrieve real-time Balances for each of an Item's accounts
+app.get('/api/balance', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+      const balanceResponse = await client.accountsBalanceGet({
+        access_token: ACCESS_TOKEN,
+      });
+      prettyPrintResponse(balanceResponse);
+      response.json(balanceResponse.data);
+    })
+    .catch(next);
+});
+
+// Retrieve information about an Item
+app.get('/api/item', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+      // Pull the Item - this includes information about available products,
+      // billed products, webhook information, and more.
+      const itemResponse = await client.itemGet({
+        access_token: ACCESS_TOKEN,
+      });
+      // Also pull information about the institution
+      const configs = {
+        institution_id: itemResponse.data.item.institution_id,
+        country_codes: PLAID_COUNTRY_CODES,
+      };
+      const instResponse = await client.institutionsGetById(configs);
+      prettyPrintResponse(itemResponse);
+      response.json({
+        item: itemResponse.data.item,
+        institution: instResponse.data.institution,
+      });
+    })
+    .catch(next);
+});
+
+// Retrieve an Item's accounts
+app.get('/api/accounts', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+      const accountsResponse = await client.accountsGet({
+        access_token: ACCESS_TOKEN,
+      });
+      prettyPrintResponse(accountsResponse);
+      response.json(accountsResponse.data);
+    })
+    .catch(next);
+});
+//-----Plaid-end------------------------------------------//
+
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(
@@ -82,13 +371,14 @@ passport.use(
     function (accessToken, refreshToken, profile, cb) {
       console.log(profile)
 
-      //capture profile id and picture
-      User.findOrCreate({ googleId: profile.id }, { profilePic: profile.photos[0].value }, function (err, user) {
+      //capture profile id 
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
         return cb(err, user)
       })
     }
   )
 )
+
 
 app.get('/', (req, res) => {
   res.render('home.ejs', { req: req });
@@ -172,8 +462,7 @@ app.post('/login', function (req, res) {
 })
 
 app.get('/index', (req, res) => {
-  // pass the profile picture URL to the template
-  res.render('home.ejs', { req: req, profilePic: req.user ? req.user.profilePic : null });
+  res.render('home.ejs', { req: req });
 });
 
 app.get('/contact', (req, res) => {
